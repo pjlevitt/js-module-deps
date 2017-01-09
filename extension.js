16f420acd824cac215b64a0de7bf720821dd1df5
Paul Levitt <paul.levitt@adp.com>
2017-01-09 21:06:31 +0000

    Initial commit

---
+++ extension.js
@@ -0,0 +1,26 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+var vscode = require('vscode');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "js-module-deps" is now active!');
+
+    // Add a browse dependencies link 
+    var browseDependencies = vscode.commands.registerCommand('extension.browseDependencies', function(node){
+        console.log(node);
+        vscode.window.showInformationMessage('node: ' + node);
+    });
+
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {
+}
+exports.deactivate = deactivate;
